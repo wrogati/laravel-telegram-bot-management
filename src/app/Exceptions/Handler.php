cf75c7cbd0fbdb3412fc2736fa735cbbbe7d22f7
alvez2g <joeltiago00745@gmail.com>
2023-01-28 13:32:41 -0300

    edit exceptions handler - wip

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -3,32 +3,24 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var string[]
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $dontFlash = [
         'current_password',
@@ -50,6 +42,13 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)
     {
-        return response()->json(['message' =>  $e->getMessage(), 'trace' => $e->getTraceAsString()], $e->getCode());
+        if (!env('APP_DEBUG')) {
+            if ($e instanceof ValidationException)
+                return response()->json(['errors' => $e->errors()], 422);
+
+            return response()->json(['errors' => $e->errors()], 422);
+        }
+
+        return parent::render($request, $e);
     }
 }
