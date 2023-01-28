2d91586fae74b5f198a0ebfb10e06cbe59ff0a57
alvez2g <joeltiago00745@gmail.com>
2023-01-28 13:34:18 -0300

    added other string cast types

--- src/app/Services/TelegramService/helpers/string.php
+++ src/app/Services/TelegramService/helpers/string.php
@@ -14,3 +14,33 @@ if (!function_exists('camelCaseToSnakeCase')) {
         return $name;
     }
 }
+
+if (!function_exists('camelCaseToDashCase')) {
+    function camelCaseToDashCase(string $string): string
+    {
+        $words = preg_split('/(?=[A-Z])/', $string);
+
+        $name = strtolower($words[0]);
+
+        for ($i = 1; $i < count($words); $i++) {
+            $name .= '-' . strtolower($words[$i]);
+        }
+
+        return $name;
+    }
+}
+
+if (!function_exists('camelCaseToPointCase')) {
+    function camelCaseToPointCase(string $string): string
+    {
+        $words = preg_split('/(?=[A-Z])/', $string);
+
+        $name = strtolower($words[0]);
+
+        for ($i = 1; $i < count($words); $i++) {
+            $name .= '.' . strtolower($words[$i]);
+        }
+
+        return $name;
+    }
+}
